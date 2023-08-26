407f68e0523efc95a4b071413d3f1df8ae184ae6
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2023-08-26 14:51:14 +0700

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -118,28 +118,372 @@ const projects = [
 	,
 	{
 		id: 0,
-		title: 'Rubicon',
-		category: 'DEX',
-		img: require('@/assets/images/projects/rubicon.png'),
-		url: 'https://rubicon.finance/'
+		title: 'Debank',
+		category: 'Web3 Social',
+		img: require('@/assets/images/projects/debank.png'),
+		url: 'https://debank.com/'
 	}
 	,
 	{
 		id: 0,
-		title: 'Sushi',
-		category: 'DEX',
-		img: require('@/assets/images/projects/sushi.png'),
-		url: 'https://www.sushi.com/'
+		title: 'Celer Network',
+		category: 'Bridge',
+		img: require('@/assets/images/projects/celer-network.png'),
+		url: 'https://celer.network/'
 	}
 	,
 	{
 		id: 0,
-		title: 'Yearn.Finance',
+		title: 'Poly Network',
+		category: 'Bridge',
+		img: require('@/assets/images/projects/poly-network.png'),
+		url: 'https://www.poly.network/#/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Orbiter Finance',
+		category: 'Bridge',
+		img: require('@/assets/images/projects/orbiter-finance.png'),
+		url: 'https://www.orbiter.finance/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Layerswap',
+		category: 'Bridge',
+		img: require('@/assets/images/projects/layerswap.png'),
+		url: 'https://www.layerswap.io/'
+	}
+	,
+	{
+		id: 0,
+		title: 'KyberSwap',
 		category: 'DeFi (other)',
-		img: require('@/assets/images/projects/yearn-finance.png'),
-		url: 'https://yearn.finance/'
+		img: require('@/assets/images/projects/kyberswap.png'),
+		url: 'https://kyberswap.com/'
 	}
-	
+	,
+	{
+		id: 0,
+		title: 'Symbiosis',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/symbiosis.png'),
+		url: 'https://symbiosis.finance/'
+	}
+	,
+	{
+		id: 0,
+		title: 'PancakeSwap',
+		category: 'DEX',
+		img: require('@/assets/images/projects/pancakeswap.png'),
+		url: 'https://pancakeswap.finance/'
+	}
+	,
+	{
+		id: 0,
+		title: 'SynFutures',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/synfutures.png'),
+		url: 'https://www.synfutures.com/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Tevaera',
+		category: 'NFT',
+		img: require('@/assets/images/projects/tevaera.png'),
+		url: 'https://tevaera.com/'
+	}
+	,
+	{
+		id: 0,
+		title: 'SpaceFi',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/spacefi.png'),
+		url: 'https://spacefi.io/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Crypto Maze',
+		category: 'NFT',
+		img: require('@/assets/images/projects/cryptomaze.png'),
+		url: 'https://cryptomaze.app/'
+	}
+	,
+	{
+		id: 0,
+		title: 'DOMANI Protocol',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/domani.png'),
+		url: 'https://dapp.domani.finance/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Onchain Trade',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/onchain-trade.png'),
+		url: 'https://onchain.trade/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Kreatorland',
+		category: 'NFT',
+		img: require('@/assets/images/projects/kreatorland.png'),
+		url: 'https://kreatorland.com/'
+	}
+	,
+	{
+		id: 0,
+		title: 'UniDex',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/unidex.png'),
+		url: 'https://www.unidex.exchange/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Meson',
+		category: 'Bridge',
+		img: require('@/assets/images/projects/meson.png'),
+		url: 'https://meson.fi/'
+	}
+	,
+	{
+		id: 0,
+		title: 'XY Finance',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/xy.png'),
+		url: 'https://xy.finance/'
+	}
+	,
+	{
+		id: 0,
+		title: 'OpenMeta',
+		category: 'NFT',
+		img: require('@/assets/images/projects/openmeta.png'),
+		url: 'https://nft.openmeta.finance/'
+	}
+	,
+	{
+		id: 0,
+		title: 'zkApe',
+		category: 'NFT',
+		img: require('@/assets/images/projects/zkape.png'),
+		url: 'https://zkape.io/#/'
+	}
+	,
+	{
+		id: 0,
+		title: 'zkRock',
+		category: 'NFT',
+		img: require('@/assets/images/projects/zkrock.png'),
+		url: 'https://www.zkrock.xyz/#/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Funkyland',
+		category: 'NFT',
+		img: require('@/assets/images/projects/funkyland.png'),
+		url: 'https://www.funkyland.org/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Galaxia Studios',
+		category: 'NFT',
+		img: require('@/assets/images/projects/galaxiastudios.png'),
+		url: 'https://www.galaxiastudios.com/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Goal3',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/goal3.png'),
+		url: 'https://beta.goal3.xyz/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Omnisea',
+		category: 'NFT',
+		img: require('@/assets/images/projects/omnisea.png'),
+		url: 'https://omnisea.org/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Maverick Protocol',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/mav.png'),
+		url: 'https://www.mav.xyz/'
+	}
+	,
+	{
+		id: 0,
+		title: 'zkSync Name Service',
+		category: 'NFT',
+		img: require('@/assets/images/projects/zkns.png'),
+		url: 'https://zkns.domains/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Stratum Exchange',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/stratumexchange.png'),
+		url: 'https://stratumexchange.com/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Dmail Network',
+		category: 'Web3 Social',
+		img: require('@/assets/images/projects/dmail.png'),
+		url: 'https://dmail.ai/'
+	}
+	,
+	{
+		id: 0,
+		title: 'zkBoost',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/zkboost.png'),
+		url: 'https://zkboost.finance/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Zomma Protocol',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/zomma.png'),
+		url: 'https://zomma.pro/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Deri Protocol',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/deri.png'),
+		url: 'https://deri.io/#/index'
+	}
+	,
+	{
+		id: 0,
+		title: 'Whisper',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/whispertools.png'),
+		url: 'https://whispertools.io/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Hue',
+		category: 'NFT',
+		img: require('@/assets/images/projects/huenft.png'),
+		url: 'https://huenft.com/'
+	}
+	,
+	{
+		id: 0,
+		title: 'ZNS Domain',
+		category: 'NFT',
+		img: require('@/assets/images/projects/zns.png'),
+		url: 'https://zns.is/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Galoswap',
+		category: 'DEX',
+		img: require('@/assets/images/projects/galoswap.png'),
+		url: 'https://galoswap.io/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Mailchain',
+		category: 'Web3 Social',
+		img: require('@/assets/images/projects/mailchain.png'),
+		url: 'https://mailchain.com/'
+	}
+	,
+	{
+		id: 0,
+		title: 'HeartX',
+		category: 'NFT',
+		img: require('@/assets/images/projects/heartx.png'),
+		url: 'https://heartx.art/explore'
+	}
+	,
+	{
+		id: 0,
+		title: 'Tiny World',
+		category: 'NFT',
+		img: require('@/assets/images/projects/tinyworlds.png'),
+		url: 'https://tinyworlds.io/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Galaxyblitz',
+		category: 'NFT',
+		img: require('@/assets/images/projects/galaxyblitz.png'),
+		url: 'https://galaxyblitz.world/'
+	}
+	,
+	{
+		id: 0,
+		title: 'zkVeggies',
+		category: 'NFT',
+		img: require('@/assets/images/projects/zkveggies.png'),
+		url: 'https://zkveggies.com/'
+	}
+	,
+	{
+		id: 0,
+		title: 'zkPengz',
+		category: 'NFT',
+		img: require('@/assets/images/projects/zkpengz.png'),
+		url: 'https://zkpengz.com/'
+	}
+	,
+	{
+		id: 0,
+		title: 'ZeroLend',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/zerolend.png'),
+		url: 'https://zerolend.xyz/'
+	}
+	,
+	{
+		id: 0,
+		title: 'Interport Finance',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/interport.png'),
+		url: 'https://interport.fi/'
+	}
+	,
+	{
+		id: 0,
+		title: 'zkSwap Finance',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/zkswap.png'),
+		url: 'https://zkswap.finance/'
+	}
+	,
+	{
+		id: 0,
+		title: 'zkEra Finance',
+		category: 'DeFi (other)',
+		img: require('@/assets/images/projects/zkera.png'),
+		url: 'https://zkera.fi/'
+	}
+
 ];
 
 export default projects;
